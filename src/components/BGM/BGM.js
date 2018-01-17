efa3f26e3eb4fb32578931f053972b5cd4a268bf
qiqiboy <imqiqiboy@gmail.com>
2018-01-17 11:21:42 +0800

    BGM更新

--- src/components/BGM/BGM.js
+++ src/components/BGM/BGM.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './BGM.scss';
 
 class BGM extends Component {
     state = {
         playing: true
     };
-    componentDidMount() {
-        //绑定事件以触发主动播放
-        document.addEventListener('DOMContentLoaded', this.autoplay, false);
-        document.addEventListener('WeixinJSBridgeReady', this.autoplay, false);
 
-        document.addEventListener('touchstart', this.autoplay, false);
+    componentDidMount() {
+        if (this.props.autoplay) {
+            //绑定事件以触发主动播放
+            document.addEventListener('DOMContentLoaded', this.autoplay, false);
+            document.addEventListener('WeixinJSBridgeReady', this.autoplay, false);
+
+            document.addEventListener('touchstart', this.autoplay, false);
+
+            //如果后加载组件，绑定WeixinJSBridgeReady事件不会重复触发，所以我们需要手动触发
+            if (typeof WeixinJSBridge !== 'undefined') {
+                this.autoplay();
+            }
+        }
 
         this.refs.box.addEventListener('click', this.togglePlay, false);
-
         this.refs.player.addEventListener('pause', this.onpause, false);
         this.refs.player.addEventListener('play', this.onplay, false);
-
-        //如果后加载组件，绑定WeixinJSBridgeReady事件不会重复触发，所以我们需要手动触发
-        if (typeof WeixinJSBridge !== 'undefined') {
-            this.autoplay();
-        }
     }
 
     componentWillUnmount() {
@@ -29,18 +30,26 @@ class BGM extends Component {
         this.refs.player.addEventListener('pause', this.onpause, false);
         this.refs.player.addEventListener('play', this.onplay, false);
 
-        document.removeEventListener('touchstart', this.autoplay, false);
+        if (this.props.autoplay) {
+            document.removeEventListener('touchstart', this.autoplay, false);
+        }
     }
 
     onpause = () =>
-        this.setState({
-            playing: false
-        });
+        this.setState(
+            {
+                playing: false
+            },
+            this.props.onpause
+        );
 
     onplay = () =>
-        this.setState({
-            playing: true
-        });
+        this.setState(
+            {
+                playing: true
+            },
+            this.props.onplay
+        );
 
     autoplay = ev => {
         const player = this.refs.player;
@@ -50,7 +59,6 @@ class BGM extends Component {
             player.paused //且没有在播放
         ) {
             //该功能依赖于已经验证过WeixinJSBridge权限
-            //只需要页面调用过 share.init() 即可，该操作会自动去微信验证WeixinJSBridge调用权限
             if (typeof WeixinJSBridge !== 'undefined') {
                 window.WeixinJSBridge.invoke('getNetworkType', {}, function(e) {
                     player.play();
@@ -75,16 +83,37 @@ class BGM extends Component {
     };
 
     render() {
+        const { playing } = this.state;
+        const { className, autoplay, loop, component } = this.props;
+        let children = component || this.props.children;
+
+        if (typeof children === 'function') {
+            const Children = children;
+
+            children = <Children playing={playing} paused={!playing} />;
+        }
+
         return (
-            <div className={'bgm-box' + (this.state.playing ? ' bgm-playing' : '')} ref="box">
-                <audio autoPlay loop src={this.props.src} className="player" ref="player" />
-                <div className="music" />
+            <div className={(className ? className + ' ' : '') + 'bgm-box ' + (this.state.playing ? 'bgm-playing' : 'bgm-paused')} ref="box">
+                <audio autoPlay={autoplay} loop={loop} src={this.props.src} className="player" ref="player" />
+                {children || <div className="music" />}
             </div>
         );
     }
 
+    static defaultProps = {
+        autoplay: true,
+        loop: true
+    };
+
     static propTypes = {
-        src: PropTypes.string.isRequired
+        src: PropTypes.string.isRequired,
+        autoplay: PropTypes.bool,
+        loop: PropTypes.bool,
+        onplay: PropTypes.func,
+        onpause: PropTypes.func,
+        component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+        children: PropTypes.oneOfType([PropTypes.node, PropTypes.func])
     };
 }
 
--- src/components/BGM/README.md
+++ src/components/BGM/README.md
@@ -1,24 +1,42 @@
-# 带开关的背景音乐播放组件
+# 背景音乐播放组件
 
 可以生成带开关的可以自动播放的（支持微信自动播放）的组件 http://t.cn/RQxWTFd
 
 ### 安装
-该方式不推荐使用，建议直接复制本代码进项目中，根据需求定制。
+
 ```
 npm install react-awesome-snippets-bgm --save
 ```
+
+### 属性参数
+
+* **{String} src** 音频文件地址
+* **{Boolean} [autoplay]** 是否自动播放
+* **{Boolean} [loop]** 是否循环播放
+* **{Function} [onplay]** 开始播放时回调（包括从暂停中恢复）
+* **{Function} [onpause]** 暂停时回调
+* **{Node|Function} [component]** 要渲染的节点，可以传递 react element，也可以传递组件构建函数，该组件将会获得从上级传递下来的播放状态（playing paused）
+
+`component`存在的话会覆盖`children`属性。
 
 ### 使用
 
 ```javascript
 import React from 'react';
 import { render } from 'react-dom';
-//源码调用
-import BGM from 'YOUR_COMPONENTS_DIR/BGM';
-//or 安装npm包调用
 import BGM from 'react-awesome-snippets-bgm';
 
-import bgmp3 from 'YOUR_MEDIA_DIR/bgm.mp3';
+import bgmp3 from 'YOUR_MEDIA_DIR/bgm.mp3'; //你的音频文件地址
 
-render(<BGM src={bgmp3} />, document.getElementById('root'));
+//渲染带状态的节点
+render(
+    <BGM src={bgmp3}>{({ playing }) => <div className={playing ? 'playing' : 'paused'} />}</BGM>,
+    document.getElementById('root')
+);
+
+//效果同上
+render(
+    <BGM src={bgmp3} component={({ playing }) => <div className={playing ? 'playing' : 'paused'} />} />,
+    document.getElementById('root')
+);
 ```
