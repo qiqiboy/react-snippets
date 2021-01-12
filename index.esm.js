6a4174d765ba21c9eda08cae5bd9039e46095317
Jiaqi Liu <qiqiboy@itiger.com>
2021-01-12 20:33:59 +0800

    build: + react-awesome-snippets@0.0.24

---
+++ index.esm.js
@@ -0,0 +1,7 @@
+export { default as CreatePic } from 'react-awesome-snippets-create-pic';
+export { default as BGM } from 'react-awesome-snippets-bgm';
+export { default as Preload } from 'react-awesome-snippets-preload';
+export { default as classnames } from 'react-awesome-snippets-classnames';
+export { default as BlinkAnimation } from 'react-awesome-snippets-blink';
+export { default as withTransition } from 'react-awesome-snippets-with-transition';
+export { default as AnimatedRouter } from 'react-animated-router';
--- index.js
+++ index.js
@@ -1,70 +1,7 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _reactAwesomeSnippetsCreatePic = require('react-awesome-snippets-create-pic');
-
-Object.defineProperty(exports, 'CreatePic', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAwesomeSnippetsCreatePic).default;
-  }
-});
-
-var _reactAwesomeSnippetsBgm = require('react-awesome-snippets-bgm');
-
-Object.defineProperty(exports, 'BGM', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAwesomeSnippetsBgm).default;
-  }
-});
-
-var _reactAwesomeSnippetsPreload = require('react-awesome-snippets-preload');
-
-Object.defineProperty(exports, 'Preload', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAwesomeSnippetsPreload).default;
-  }
-});
-
-var _reactAwesomeSnippetsClassnames = require('react-awesome-snippets-classnames');
-
-Object.defineProperty(exports, 'classnames', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAwesomeSnippetsClassnames).default;
-  }
-});
-
-var _reactAwesomeSnippetsBlink = require('react-awesome-snippets-blink');
-
-Object.defineProperty(exports, 'BlinkAnimation', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAwesomeSnippetsBlink).default;
-  }
-});
-
-var _reactAwesomeSnippetsWithTransition = require('react-awesome-snippets-with-transition');
-
-Object.defineProperty(exports, 'withTransition', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAwesomeSnippetsWithTransition).default;
-  }
-});
-
-var _reactAnimatedRouter = require('react-animated-router');
-
-Object.defineProperty(exports, 'AnimatedRouter', {
-  enumerable: true,
-  get: function get() {
-    return _interopRequireDefault(_reactAnimatedRouter).default;
-  }
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+exports.CreatePic = require('react-awesome-snippets-create-pic');
+exports.BGM = require('react-awesome-snippets-bgm');
+exports.Preload = require('react-awesome-snippets-preload');
+exports.classnames = require('react-awesome-snippets-classnames');
+exports.BlinkAnimation = require('react-awesome-snippets-blink');
+exports.withTransition = require('react-awesome-snippets-with-transition');
+exports.AnimatedRouter = require('react-animated-router');
